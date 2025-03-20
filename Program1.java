47a3c8a97602eaaee2624e099b6fbaed3cfd5a9d
UEM <debangshu.mandal2003@gmail.com>
2025-03-20 14:37:48 +0530

    changed logic

--- Program1.java
+++ Program1.java
@@ -2,9 +2,9 @@ public class Program1{
 	public static void main(String[] args){
 		int a = 10;
 		int b = 20;
-		int temp = a;
-		a = b;
-		b = temp;
+		a = a*b;
+		b = a\b;
+		a = a\b;
 		System.out.println(a);
 		System.out.println(b);
 	}
